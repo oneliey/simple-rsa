606df2744c5d16aa3549bb7f5386d51311de5f93
Oneliey <oneliey@outlook.com>
2021-11-11 16:20:06 +0800

    Add build.sh for 4 os/arch:  darwin/arm64, darwin/amd64, linux/amd64, windows/amd64

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+
+# darwin/arm64
+GOOS=darwin GOARCH=arm64 go build -o ./bin/darwin/simpleRSA-darwin-arm64
+GOOS=darwin GOARCH=arm64 go build -o bin/simpleRSA.app/Contents/MacOS/simpleRSA
+
+# darwin/amd64
+GOOS=darwin GOARCH=amd64 go build -o ./bin/darwin/simpleRSA-darwin-amd64
+GOOS=darwin GOARCH=amd64 go build -o bin/simpleRSA-amd64.app/Contents/MacOS/simpleRSA-amd64
+
+# linux/amd64
+GOOS=linux GOARCH=amd64 go build -o ./bin/linux/simpleRSA-linux-amd64
+
+# windows/amd64
+GOOS=windows GOARCH=amd64 go build -o ./bin/windows/simpleRSA-windows-amd64.exe
